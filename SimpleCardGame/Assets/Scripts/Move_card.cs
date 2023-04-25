f47e79bacd169e61ba7e3509da0b789ce3a5671b
YoungMin Cho (Luke) <83196845+luke-youngmin-cho@users.noreply.github.com>
2023-04-25 19:50:22 +0900

    1st_commit

---
+++ SimpleCardGame/Assets/Scripts/Move_card.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_card : MonoBehaviour
+{
+    Rigidbody2D my_rigid;
+
+    Vector2 my_position;
+    Vector2 goal_position;
+
+    
+    public GameObject goal;
+
+    
+
+    void Start()
+    {
+        my_rigid = GetComponent<Rigidbody2D>();
+        my_position = transform.position;
+
+        goal_position = goal.transform.position;
+        
+
+        Debug.Log("my_position : " + my_position);
+        Debug.Log("goal_position : " + goal_position);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+}
