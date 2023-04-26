8057e987037bb69add4a0e9ef7c295c8ab9eabc4
YoungMin Cho (Luke) <83196845+luke-youngmin-cho@users.noreply.github.com>
2023-04-26 19:53:17 +0900

    move_card

--- SimpleCardGame/Assets/Scripts/Move_card.cs
+++ SimpleCardGame/Assets/Scripts/Move_card.cs
@@ -6,32 +6,43 @@ public class Move_card : MonoBehaviour
 {
     Rigidbody2D my_rigid;
 
-    Vector2 my_position;
-    Vector2 goal_position;
+    Vector3 my_position;
+    Vector3 goal_position;
+    Vector3 next_position;
 
     [SerializeField]
     GameObject goal;
 
     
-
     void Start()
     {
         my_rigid = GetComponent<Rigidbody2D>();
         my_position = transform.position;
 
         goal_position = goal.transform.position;
+
+        next_position = goal_position - my_position;
+
         
 
         Debug.Log("my_position : " + my_position);
         Debug.Log("goal_position : " + goal_position);
-
+        Debug.Log("changeposition : " + next_position);
+        Debug.Log("next_position¿« normal : " + next_position.normalized);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        next_position = goal_position - transform.position;
+        //transform.position = transform.position + goal_position.normalized * Time.deltaTime;
+        //transform.position = transform.position + next_position.normalized * Time.deltaTime;
+
+        //my_rigid.MovePosition(transform.position + next_position.normalized *15* Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, goal_position, 3 * Time.deltaTime);
+
     }
 
 
