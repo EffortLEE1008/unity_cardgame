998497e32cdbe05c2296dc5dbe974ecc5e415ecf
YoungMin Cho (Luke) <83196845+luke-youngmin-cho@users.noreply.github.com>
2023-04-28 19:05:08 +0900

    change_array, poker_card_logic

--- SimpleCardGame/Assets/Scripts/GameManager.cs
+++ SimpleCardGame/Assets/Scripts/GameManager.cs
@@ -11,11 +11,19 @@ public class GameManager : MonoBehaviour
     public GameObject card4;
     public GameObject card5;
 
+    [SerializeField]
+    GameObject[] card_array; 
+
+
     SpriteRenderer card1_renderer;
     SpriteRenderer card2_renderer;
     SpriteRenderer card3_renderer;
     SpriteRenderer card4_renderer;
     SpriteRenderer card5_renderer;
+
+    [SerializeField]
+    SpriteRenderer[] sr_array;
+    
 
     public Sprite[] my_sprites;
 
@@ -26,31 +34,61 @@ public class GameManager : MonoBehaviour
     void Start()
     {
         //렌더러를 활용하여 sprite 변경
+        for (int i = 0; i < card_array.Length; i++)
+        {
+            sr_array[i] = card_array[i].GetComponent<SpriteRenderer>();
+            sr_array[i].sprite = my_sprites[i];
+            card_list.Add(sr_array[i].sprite.name);
+            card_list_numonly.Add(int.Parse(sr_array[i].sprite.name.Substring(0, 2)));
+            card_list_shapeonly.Add(sr_array[i].sprite.name.Substring(2, 1));
+        }
+
+
+
         card1_renderer = card1.GetComponent<SpriteRenderer>();
         card1_renderer.sprite = my_sprites[0];
 
         //리스트에추가
         card_list.Add(card1_renderer.sprite.name);
         card_list_numonly.Add(int.Parse(card1_renderer.sprite.name.Substring(0, 2)));
+        card_list_shapeonly.Add(card1_renderer.sprite.name.Substring(2, 1));
 
 
         card2_renderer = card2.GetComponent<SpriteRenderer>();
         card2_renderer.sprite = my_sprites[1];
 
+        card_list.Add(card2_renderer.sprite.name);
+        card_list_numonly.Add(int.Parse(card2_renderer.sprite.name.Substring(0, 2)));
+        card_list_shapeonly.Add(card2_renderer.sprite.name.Substring(2, 1));
+
 
         card3_renderer = card3.GetComponent<SpriteRenderer>();
         card3_renderer.sprite = my_sprites[2];
 
+        card_list.Add(card3_renderer.sprite.name);
+        card_list_numonly.Add(int.Parse(card3_renderer.sprite.name.Substring(0, 2)));
+        card_list_shapeonly.Add(card3_renderer.sprite.name.Substring(2, 1));
+
 
         card4_renderer = card4.GetComponent<SpriteRenderer>();
         card4_renderer.sprite = my_sprites[3];
+        card_list.Add(card4_renderer.sprite.name);
+        card_list_numonly.Add(int.Parse(card4_renderer.sprite.name.Substring(0, 2)));
+        card_list_shapeonly.Add(card4_renderer.sprite.name.Substring(2, 1));
 
 
         card5_renderer = card5.GetComponent<SpriteRenderer>();
         card5_renderer.sprite = my_sprites[4];
 
+        card_list.Add(card5_renderer.sprite.name);
+        card_list_numonly.Add(int.Parse(card5_renderer.sprite.name.Substring(0, 2)));
+        card_list_shapeonly.Add(card5_renderer.sprite.name.Substring(2, 1));
 
-        Debug.Log(card5_renderer.sprite.name);
+        Debug.Log(card_list[0] + " " + card_list[1] + " " + card_list[2] + " "
+            + card_list[3]+ " " + card_list[4]);
+
+
+
 
     }
 
---
+++ SimpleCardGame/Poker_card_Program.cs
@@ -0,0 +1,150 @@
+﻿
+//                  0     1     2      3      4       5      6      7
+using System;
+                    
+string[] deck = {"06c", "07c", "08c", "09c", "10c", "11c", "12c", "13c",
+                 "06d", "07d", "08d", "09d", "10d", "11d", "12d", "13d",
+                 "06h", "07h", "08h", "09h", "10h", "11h", "12h", "13h",
+                 "06s", "07s", "08s", "09s", "10s", "11s", "12s", "13s"};
+
+List<string> mycard = new List<string>();
+List<int> mycard_onlynum = new List<int>();
+List<string> mycard_onlyshape = new List<string>();
+
+Random rand = new Random();
+
+List<int> randnum_list = new List<int>();
+
+for (int i = 0; i < 5;)
+{
+    int randnum = rand.Next(0, 32);
+
+    if (!randnum_list.Contains(randnum))
+    {
+        
+        randnum_list.Add(randnum);
+        i++;        
+    }
+    
+}
+
+mycard.Add(deck[randnum_list[0]]); // 11d
+mycard.Add(deck[randnum_list[1]]); // 08s
+mycard.Add(deck[randnum_list[2]]); // 12c
+mycard.Add(deck[randnum_list[3]]);// 08d
+mycard.Add(deck[randnum_list[4]]);// 06d
+
+Console.WriteLine(mycard[0] + " " + mycard[1] + " " + mycard[2]+" " + mycard[3]+
+                    " " + mycard[4]);
+
+for(int i=0; i<mycard.Count; i++)
+{
+    mycard_onlynum.Add(Convert.ToInt32(mycard[i].Substring(0, 2)));
+    
+}
+
+for (int i = 0; i < mycard.Count; i++)
+{
+    mycard_onlyshape.Add(mycard[i].Substring(2,1));
+    //Console.WriteLine(mycard_onlyshape[i]);
+}
+mycard_onlynum.Sort();
+mycard_onlyshape.Sort();
+
+bool isflush = false;
+bool ismount = false;
+
+if (mycard_onlyshape[0]== mycard_onlyshape[mycard_onlyshape.Count - 1])
+{
+    isflush = true;
+}
+
+if((mycard_onlynum[0]==6) && (mycard_onlynum[1]==10) && (mycard_onlynum[2] == 11)&&
+    (mycard_onlynum[3] == 12) && (mycard_onlynum[4] == 13))
+{
+    ismount = true;
+    
+}
+
+
+bool isstraight = false;
+
+
+for (int i = 0; i < mycard_onlynum.Count-1; i++)
+{
+    if(mycard_onlynum[i+1] - mycard_onlynum[i] == 1)
+    {
+        isstraight = true;
+        
+    }
+    else
+    {
+        isstraight = false;
+        break;
+    }
+
+}
+
+int paircount = 0;
+
+for(int i =0; i<mycard_onlynum.Count-1; i++)
+{
+    for(int j=i+1; j< mycard_onlynum.Count; j++)
+    {
+        
+
+        if (mycard_onlynum[i] == mycard_onlynum[j])
+        {
+            paircount++;
+        }
+
+    }
+
+}
+
+
+if (isflush && ismount)
+{
+    Console.WriteLine("축하합니다. 로티플이에요");
+}
+else if (isflush && isstraight)
+{
+    Console.WriteLine("축하합니다 스트레이트 플러쉬에요");
+}
+else if (paircount == 6)
+{
+    Console.WriteLine("축하합니다 포카드입니다.");
+}
+else if (paircount == 4)
+{
+    Console.WriteLine("축하합니다 풀하우스에요.");
+}
+else if (isflush) 
+{
+    Console.WriteLine("축하합니다 플러쉬에요");
+}
+else if (ismount)
+{
+    Console.WriteLine("축하합니다 마운틴입니다.");
+}
+else if (isstraight)
+{
+    Console.WriteLine("축하합니다 스트레이트입니다.");
+}
+else if (paircount==3)
+{
+    Console.WriteLine("축하합니다 트리플입니다.");
+}
+else if (paircount==2)
+{
+    Console.WriteLine("아쉽네요 투페어");
+}
+else if (paircount==1)
+{
+    Console.WriteLine("아쉽네요 원페어");
+}
+else
+{
+    Console.WriteLine("아무것도 아닌 탑카드에요");
+}
+
