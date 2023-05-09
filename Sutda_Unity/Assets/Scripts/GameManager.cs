22aabe46554418a8cac37219a4bab3153adec5f5
YoungMin Cho (Luke) <83196845+luke-youngmin-cho@users.noreply.github.com>
2023-05-09 19:53:41 +0900

    study_corutine

--- Sutda_Unity/Assets/Scripts/GameManager.cs
+++ Sutda_Unity/Assets/Scripts/GameManager.cs
@@ -1,26 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class GameManager : MonoBehaviour
 {
-    public GameObject card0;
-    public GameObject target0;
+    public GameObject[] card;
+    public GameObject[] target;
+
+    float timer = 0;
 
     void Start()
     {
+
         
-        //target구하고
-        
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        card0.transform.position = Vector3.MoveTowards(card0.transform.position, 
-                                        target0.transform.position, 3*Time.deltaTime);
+
+        for (int i = 0; i < card.Length; i++)
+        {
+            card[i].transform.position = Vector3.MoveTowards(card[i].transform.position,
+                                target[i].transform.position, 3 * Time.deltaTime);
+
+        }
+
 
 
     }
+
+    public void GameStart()
+    {
+        //to do 작성
+        //StartCoroutine(MoveCard( ));       
+ 
+    }
+
+    IEnumerator MoveCard(GameObject my_card, GameObject goal)//GameObject my_card, GameObject goal
+    {
+
+        while (timer <= 5)
+        {
+            timer = timer + Time.deltaTime;
+            my_card.transform.position = Vector3.MoveTowards(my_card.transform.position, goal.transform.position,
+                                                                   5*Time.deltaTime);
+
+            yield return null;
+        }
+        if (timer >= 5)
+        {
+            Debug.Log("timer가 5를 넘겼습니다.");
+        }
+
+
+    }
+
+   
+
+
+
 }
